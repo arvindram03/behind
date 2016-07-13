41e8d68ddf77b3822853aa021c4f6fece0ce5f6a
Arvind Ram Anantharam <arvindram03@gmail.com>
2016-07-13 17:31:53 -0500

    Adding arg parsing

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
 
+  "github.com/urfave/cli"
+)
+
+func analyse(args []string) {
+  for _, arg := range args {
+    fmt.Println("Analysing...",arg)
+  }
+
+}
 func main() {
-  fmt.Println("Hello Behind")
+  app := cli.NewApp()
+  app.Name = "Behind"
+  app.Usage = "know how behind your packages are"
+  app.Action = func(c *cli.Context) error {
+    fmt.Println("Hello Devs")
+    if !c.Args().Present() {
+        return cli.NewExitError("No input files to analyse", 1)
+    }
+
+    args := c.Args()
+    analyse(args)
+    return nil
+  }
+
+  app.Run(os.Args)
 }
